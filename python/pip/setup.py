ef3bf152441e0ac4e2927a8e81d92d2804dedef7
curoky <cccuroky@gmail.com>
2022-01-30 16:25:30 +0800

    feat(build-system/pypi): add pip example

---
+++ python/pip/setup.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+#
+# This file is part of learn-build-system.
+# See https://github.com/curoky/learn-build-system for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import os
+import sys
+from pathlib import Path
+from shutil import rmtree
+
+from pkg_resources import parse_requirements
+from setuptools import Command, find_packages, setup
+
+# Package meta-data.
+NAME = 'my_own_x'
+DESCRIPTION = 'My python utils library.'
+URL = 'https://github.com/curoky/my-own-x'
+EMAIL = 'cccuroky@gmail.com'
+AUTHOR = 'curoky'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '1.0.0'
+
+CURRENT_PATH = Path(__file__).parent
+
+# What packages are required for this module to be executed?
+# https://stackoverflow.com/questions/49689880/proper-way-to-parse-requirements-file-after-pip-upgrade-to-pip-10-x-x/59971236#59971236
+REQUIRED = [str(req) for req in parse_requirements((CURRENT_PATH / 'requirements.txt').read_text())]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+long_description = (CURRENT_PATH / 'README.md').read_text()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace('-', '_').replace(' ', '_')
+    exec((CURRENT_PATH / project_slug / '__version__.py').read_text(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print(f'\033[1m{s}\033[0m')
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree((CURRENT_PATH / 'dist').as_posix())
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(f'{sys.executable} setup.py sdist bdist_wheel --universal')
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system(f"git tag v{about['__version__']}")
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=['*_test.py'
+                                    'tests', '*.tests', '*.tests.*', 'tests.*']),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='Apache License 2.0',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
