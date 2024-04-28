ecb6817c41a16c63919bd2cf89cbf4bfe7e39830
curoky <cccuroky@gmail.com>
2024-04-28 15:20:40 +0800

    feat(bazel): update example

---
+++ bazel/with-clang-v2/src/main.cc
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018-2023 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-build-system.
+ * See https://github.com/curoky/learn-build-system for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <format>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << std::format(
+      "__clang__: {}\n"
+      "__clang_major__: {}\n"
+      "__clang_minor__: {}\n"
+      "__clang_patchlevel__: {}\n"
+      "__clang_version__: {}\n",
+      __clang__,             // set to 1 if compiler is clang
+      __clang_major__,       // integer: major marketing version number of clang
+      __clang_minor__,       // integer: minor marketing version number of clang
+      __clang_patchlevel__,  // integer: marketing patch level of clang
+      __clang_version__      // string: full version number
+  );
+  return 0;
+}
---
+++ bazel/with-libllvm-v2/src/main.cc
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2018-2023 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-build-system.
+ * See https://github.com/curoky/learn-build-system for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendAction.h>
+#include <clang/Tooling/Tooling.h>
+
+class FindNamedClassVisitor : public clang::RecursiveASTVisitor<FindNamedClassVisitor> {
+ public:
+  explicit FindNamedClassVisitor(clang::ASTContext *Context) : Context(Context) {}
+
+  bool VisitCXXRecordDecl(clang::CXXRecordDecl *Declaration) {
+    if (Declaration->getQualifiedNameAsString() == "n::m::C") {
+      clang::FullSourceLoc FullLocation = Context->getFullLoc(Declaration->getBeginLoc());
+      if (FullLocation.isValid())
+        llvm::outs() << "Found declaration at " << FullLocation.getSpellingLineNumber() << ":"
+                     << FullLocation.getSpellingColumnNumber() << "\n";
+    }
+    return true;
+  }
+
+ private:
+  clang::ASTContext *Context;
+};
+
+class FindNamedClassConsumer : public clang::ASTConsumer {
+ public:
+  explicit FindNamedClassConsumer(clang::ASTContext *Context) : Visitor(Context) {}
+
+  virtual void HandleTranslationUnit(clang::ASTContext &Context) {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+
+ private:
+  FindNamedClassVisitor Visitor;
+};
+
+class FindNamedClassAction : public clang::ASTFrontendAction {
+ public:
+  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &Compiler,
+                                                                llvm::StringRef InFile) {
+    return std::make_unique<FindNamedClassConsumer>(&Compiler.getASTContext());
+  }
+};
+
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    clang::tooling::runToolOnCode(std::make_unique<FindNamedClassAction>(), argv[1]);
+  }
+}
