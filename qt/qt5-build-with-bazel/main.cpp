82ee2808c72aaa3d1cd2a9d0d958f3f4a56a6e7e
curoky <cccuroky@gmail.com>
2024-05-06 19:37:13 +0800

    feat(qt): add cmake and bazel example

---
+++ qt/qt5-build-with-bazel/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-qt.
+ * See https://github.com/curoky/learn-qt for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <QtCore/QCoreApplication>
+#include <QtWidgets/QApplication>
+
+#include "mainwindow.h"
+
+int main(int argc, char *argv[]) {
+  QCoreApplication::setOrganizationName("com.curoky");
+  QCoreApplication::setApplicationName("HelloWorld");
+
+  QApplication a(argc, argv);
+  MainWindow w;
+
+  w.show();
+  return a.exec();
+}
---
+++ qt/qt5-build-with-cmake/mainwindow.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-qt.
+ * See https://github.com/curoky/learn-qt for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "mainwindow.h"
+
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() { delete ui; }
